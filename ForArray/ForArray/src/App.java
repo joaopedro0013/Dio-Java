a258510e6b386df738783144b120a564f22062e3
joaopedro0013 <joaopedro2002trabalho@gmail.com>
2024-10-07 21:34:45 -0300

    Estruturas de repeticao

---
+++ ForArray/ForArray/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+            for (int x=0; x<alunos.length; x++) {
+            System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+        }
+
+    }
+}
---
+++ Switch/SwitchTeste/src/App.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        String atividade = "estudar";
+
+        switch (atividade) {
+
+            case "estudar" :{
+                System.out.println("eu vou estudar!");
+                break;
+            }
+
+            case "jogar" :{
+                System.out.println("eu vou jogar!");
+                break;
+            }
+
+            case "videos" :{
+                System.out.println("eu vou ver videos!");
+                break;
+            }
+
+        }
+    }
+}
